7ede7373707ccae08294fbf52467c80a67924af8
Tuan Luong <tuanluongkt1907@gmail.com>
2025-02-11 18:10:10 +0700

    Add quiz

--- database/server.js
+++ database/server.js
@@ -8,7 +8,14 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
+server.use((err, req, res, next) => {
+  console.error("🔥 ERROR:", err);
+  res.status(500).json({ error: "Internal Server Error", details: err.message });
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
+
+
